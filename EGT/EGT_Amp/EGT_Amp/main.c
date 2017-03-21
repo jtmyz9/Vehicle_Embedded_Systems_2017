37a12bb08bdbdaf285e488b5984bb42c635d1d2d
jtmyz9 <jtmyz9@mail.missouri.edu>
2017-03-20 23:42:14 -0500

    DEBUG
    Began Debug block for uart messages.

--- EGT/EGT_Amp/EGT_Amp/main.c
+++ EGT/EGT_Amp/EGT_Amp/main.c
@@ -7,25 +7,30 @@
 
 #include <avr/io.h>
 //
+#ifdef DEBUG
+	#define FOSC 8000 // In KHz
+	#define LIN_BAUDRATE 9600 // In bit/s
+	#define CONF_LINBRR 25
 
-#define FOSC 8000 // In KHz
-#define LIN_BAUDRATE 9600 // In bit/s
-#define CONF_LINBRR 25
+	#define Lin_set_baudrate(br) \
+	{ LINBRRH = (unsigned char)(((unsigned short)br)>>8);\
+	LINBRRL = (unsigned char) ((unsigned short)br); }
 
-#define Lin_set_baudrate(br) \
-{ LINBRRH = (unsigned char)(((unsigned short)br)>>8);\
-LINBRRL = (unsigned char) ((unsigned short)br); }
+	#define Lin_set_data(data) ( LINDAT = data )
 
-#define Lin_set_data(data) ( LINDAT = data )
+	void uart_put_string (unsigned char *data_string)
+	{
+		while(*data_string++){ 
+			Lin_set_data(*data_string);
+			//data_string++;
+			};
+	}
+#endif
 
-void uart_put_string (unsigned char *data_string)
-{
-	while(*data_string++){ 
-		Lin_set_data(*data_string);
-		//data_string++;
-		};
+void init_systimer(){
+
 }
-
+	
 int main(void)
 {
 	DDRD = (1<<DDD3);
@@ -40,23 +45,37 @@ int main(void)
 	CANGIT = 0xFF;                      //! Reset General errors and OVRTIM flag
 	can_clear_all_mob();				//Clear all Mobs
 	////end CAN Bus init
-	
-//	LINCR = (1<<LENA) | (1<<LCMD2) | (1<<LCMD1) | (1<<LCMD0) | (0<<LCONF1) | (0<<LCONF0);
-//	LINCR = 0b00001111;
-	//LINBRR = 25;
-//	Lin_set_baudrate(CONF_LINBRR);
-//	unsigned char *i = "test\n";
-	
-    /* Replace with your application code */
-//	U16 result =-1;
+
+#ifdef DEBUG	
+	LINCR = (1<<LENA) | (1<<LCMD2) | (1<<LCMD1) | (1<<LCMD0) | (0<<LCONF1) | (0<<LCONF0);
+	LINCR = 0b00001111;
+	LINBRR = 25;
+	Lin_set_baudrate(CONF_LINBRR);
+	unsigned char *i = "test\n";
+#endif	
+
+   
+	U16 result =-1;
     while (1)
     {
-//		adc_init(AVCC_AS_VREF, NO_LEFT_ADJUST, 5);
-//		result = adc_single_conversion(5);
-			//uart_put_string(i);
+
+	adc_init(AVCC_AS_VREF, NO_LEFT_ADJUST, 5);
+	result = adc_single_conversion(5);
+			uart_put_string(i);
 				
 		
 		
     }
+}
+
+/*
+Interrrupt Service routine for timer compare match 
+	based on setting in init_systimer
+
+At each interrupt handles sending Thermocouple Measurements 
+	to Can Controller Mob
+*/
+ISR(TCOMP_1){
+
 }
 
